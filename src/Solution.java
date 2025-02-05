de87155a420936a20cbdde24e16a3e22ce14752d
luoyu <2085324623@qq.com>
2025-02-05 15:23:47 +0800

    twosum

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,15 @@
-public class Add {
-    
-    class Solution {
+import java.util.Arrays;
+
+class Solution {
+        public static void main(String[] args) {
+            int [] nums = {2,5,8,7,8,7,4};
+            int target = 10;
+            Solution add = new Solution();
+            int[] ints = add.twoSum(nums, target);
+            String string = Arrays.toString(ints);
+            System.out.println(string);
+        }
+
         public int[] twoSum(int[] nums, int target) {
             for (int i = 0; i < nums.length; i++) {
                 for (int j = i + 1; j < nums.length; j++) {
@@ -12,4 +21,4 @@ public class Add {
             return null;
         }
     }
-}
+
